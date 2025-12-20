5397596e0cc2858c42ff6d068fbc4092cc58f12b
diana-prom <d.tinajero0200@gmail.com>
2025-12-20 14:42:44 -0800

    feat(be): add ResponseEntity to food controller to control HTTP headers

--- backend/app/src/main/java/com/dashboard/nutrition/controller/FoodController.java
+++ backend/app/src/main/java/com/dashboard/nutrition/controller/FoodController.java
@@ -2,8 +2,10 @@ package com.dashboard.nutrition.controller;
 
 import com.dashboard.nutrition.entity.Food;
 import com.dashboard.nutrition.service.FoodService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/food")
@@ -18,5 +20,11 @@ public class FoodController {
     @GetMapping("/search")
     public Food getFoodMacros(@RequestParam String name){
         return foodService.getFoodWithMacros(name);
+    }
+
+    @GetMapping("/search/all")
+    public ResponseEntity<List<Food>> searchFood(@RequestParam String name) {
+        List<Food> foods = foodService.searchFoodsWithMacros(name);
+        return ResponseEntity.ok(foods);
     }
 }
