c75d0034c4b14a28723cf327495239aa9edfc626
diana-prom <d.tinajero0200@gmail.com>
2025-12-28 10:41:08 -0800

    feat(be): implement global CORS config in main class

--- backend/app/src/main/java/com/dashboard/nutrition/NutritionApplication.java
+++ backend/app/src/main/java/com/dashboard/nutrition/NutritionApplication.java
@@ -2,6 +2,9 @@ package com.dashboard.nutrition;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class NutritionApplication {
@@ -10,4 +13,16 @@ public class NutritionApplication {
 		SpringApplication.run(NutritionApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**") // allow all endpoints
+						.allowedOrigins("http://localhost:3000", "http://frontend:80") // frontend container or localhost dev
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*");
+			}
+		};
+	}
 }
