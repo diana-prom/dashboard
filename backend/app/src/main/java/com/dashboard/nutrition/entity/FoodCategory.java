64d5672fa4959f61de042a68a73686198c6aaf72
diana-prom <d.tinajero0200@gmail.com>
2025-12-28 09:31:54 -0800

    feat(be): add food category entity to map to food_category table in MySQL

---
+++ backend/app/src/main/java/com/dashboard/nutrition/entity/FoodCategory.java
@@ -0,0 +1,44 @@
+package com.dashboard.nutrition.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(
+        name = "food_category"
+)
+public class FoodCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "code")
+    private Integer code;
+
+    @Column(name = "description")
+    private String description;
+
+    public FoodCategory() {
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getCode() {
+        return this.code;
+    }
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
